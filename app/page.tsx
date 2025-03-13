dddeca523094b430ad7e0343584538c86721f1f0
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2025-03-13 21:34:18 +0530

    updated

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black">
+      <div>
+        <h1>hello portfolio</h1>
+      </div>
+    </main>
+  );
+}
