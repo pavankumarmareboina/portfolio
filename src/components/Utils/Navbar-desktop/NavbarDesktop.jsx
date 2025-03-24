84d781c4875a422abfb5ee4cf1c54117465d946b
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2025-03-24 23:08:57 +0530

    Update NavbarDesktop.jsx

--- src/components/Utils/Navbar-desktop/NavbarDesktop.jsx
+++ src/components/Utils/Navbar-desktop/NavbarDesktop.jsx
@@ -1,4 +1,45 @@
+// import React from "react";
+// import "./NavbarDesktop.css";
+// import LogoDesktop from "../../../assets/Navbar-Img/logoDesktop.svg";
+// import LightModeIcon from "../../../assets/Navbar-Img/LightModeIcon.svg";
+
+// const NavbarDesktop = () => {
+//   return (
+//     <nav>
+//       <div className="navbar">
+//         <div className="logo">
+//           <img src={LogoDesktop} alt="Logo" />
+//         </div>
+//         <ul className="navLinks">
+//           <li>
+//             <a href="/">Home</a>
+//           </li>
+//           <li>
+//             <a href="/Project">Project</a>
+//           </li>
+//           <li>
+//             <a href="/About">About</a>
+//           </li>
+//           <li>
+//             <a href="/Blog">Blog</a>
+//           </li>
+//           <li>
+//             <a href="/Contact">Contact</a>
+//           </li>
+//         </ul>
+//         <div className="ToggleMode">
+//           <img src={LightModeIcon} alt="Logo" />
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default NavbarDesktop;
+
+
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import "./NavbarDesktop.css";
 import LogoDesktop from "../../../assets/Navbar-Img/logoDesktop.svg";
 import LightModeIcon from "../../../assets/Navbar-Img/LightModeIcon.svg";
@@ -12,23 +53,23 @@ const NavbarDesktop = () => {
         </div>
         <ul className="navLinks">
           <li>
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/Project">Project</a>
+            <Link to="/Project">Project</Link>
           </li>
           <li>
-            <a href="/About">About</a>
+            <Link to="/About">About</Link>
           </li>
           <li>
-            <a href="/Blog">Blog</a>
+            <Link to="/Blog">Blog</Link>
           </li>
           <li>
-            <a href="/Contact">Contact</a>
+            <Link to="/Contact">Contact</Link>
           </li>
         </ul>
         <div className="ToggleMode">
-          <img src={LightModeIcon} alt="Logo" />
+          <img src={LightModeIcon} alt="Theme Toggle" />
         </div>
       </div>
     </nav>
